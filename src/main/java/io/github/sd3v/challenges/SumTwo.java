5e853549fcb7d43f4b4712591313badf842cba12
Sergio Cano <canosergio90@gmail.com>
2023-08-31 22:26:35 +0200

    sum two

---
+++ src/main/java/io/github/sd3v/challenges/SumTwo.java
@@ -0,0 +1,25 @@
+package io.github.sd3v.challenges;
+
+import java.util.HashMap;
+
+public class SumTwo {
+
+    public static int[] sumTwo(int[] arr, int target) {
+        var numMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            var sumTwoNumber = target - arr[i];
+            var sumTwoIndex = numMap.get(sumTwoNumber);
+            if (sumTwoIndex != null) {
+                if (arr[i] > arr[sumTwoIndex]) {
+                    return new int[] { sumTwoIndex + 1, i + 1};
+                } else {
+                    return new int[] { i + 1, sumTwoIndex + 1};
+                }
+            }
+            else {
+                numMap.put(arr[i], i);
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}

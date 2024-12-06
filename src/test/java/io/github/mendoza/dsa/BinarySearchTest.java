6f27fffd2d8153acbd2349b9db28c976e5df0228
Sergio Cano <canosergio90@gmail.com>
2024-12-06 22:31:41 +0100

    binary search

--- src/test/java/io/github/mendoza/dsa/BinarySearchTest.java
+++ src/test/java/io/github/mendoza/dsa/BinarySearchTest.java
@@ -1,23 +1,23 @@
 package io.github.mendoza.dsa;
 
-import org.junit.jupiter.api.Test;
-
 import static io.github.mendoza.dsa.BinarySearch.binarySearch;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class BinarySearchTest {
 
-    @Test
-    public void whenItemIsFound() {
-        int[] list = { 1, 2, 5, 7, 9, 13};
-        int result = binarySearch(list, 7);
-        assertEquals(3, result);
-    }
+  @Test
+  public void whenItemIsFound() {
+    int[] list = {1, 2, 5, 7, 9, 13};
+    int result = binarySearch(list, 7);
+    assertEquals(3, result);
+  }
 
-    @Test
-    public void whenItemNotFound() {
-        int[] list = { 1, 2, 5, 7, 9, 13};
-        int result = binarySearch(list, 8);
-        assertEquals(-1, result);
-    }
+  @Test
+  public void whenItemNotFound() {
+    int[] list = {1, 2, 5, 7, 9, 13};
+    int result = binarySearch(list, 8);
+    assertEquals(-1, result);
+  }
 }

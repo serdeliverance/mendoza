79b705f7e8d8fba7cbc6bddd1b4bddbc10031375
Sergio Cano <canosergio90@gmail.com>
2026-07-21 11:10:46 -0300

    catching exception inside ComplexCalculation

--- src/main/java/io/github/mendoza/learning/corejava/threads/ComplexCalculation.java
+++ src/main/java/io/github/mendoza/learning/corejava/threads/ComplexCalculation.java
@@ -3,15 +3,20 @@ package io.github.mendoza.learning.corejava.threads;
 import java.math.BigInteger;
 
 public class ComplexCalculation {
-    public BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) throws InterruptedException {
+    public BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) {
         PowerCalculatingThread thread1 = new PowerCalculatingThread(base1, power1);
         PowerCalculatingThread thread2 = new PowerCalculatingThread(base2, power2);
 
         thread1.start();
         thread2.start();
 
-        thread1.join();
-        thread2.join();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
         BigInteger input1 = thread1.getResult();
         BigInteger input2 = thread2.getResult();

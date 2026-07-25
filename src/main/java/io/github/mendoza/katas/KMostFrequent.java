1b4be4c1da12519249649bbadec753562e8de48f
Sergio Cano <canosergio90@gmail.com>
2026-07-25 19:34:58 -0300

    k most frequent - improved solution #1

--- src/main/java/io/github/mendoza/katas/KMostFrequent.java
+++ src/main/java/io/github/mendoza/katas/KMostFrequent.java
@@ -1,6 +1,5 @@
 package io.github.mendoza.katas;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,39 +13,16 @@ import java.util.Map;
 public class KMostFrequent {
 
   public static int[] solution(int[] input, int k) {
-    if (input.length == 0) {
-      return new int[] {};
-    }
-
-    int[] sortedInput = Arrays.stream(input).boxed().sorted().mapToInt(Integer::intValue).toArray();
-
     Map<Integer, Integer> numberFrequencyMap = new HashMap<>();
 
-    int prev = sortedInput[0];
-    int i = 0;
-    int frequency = 0;
-
-    numberFrequencyMap.put(prev, frequency);
-
-    while (i < sortedInput.length) {
-      if (prev != sortedInput[i]) {
-        frequency = 0;
-        prev = sortedInput[i];
-        numberFrequencyMap.put(prev, frequency);
-      }
-
-      frequency++;
-      numberFrequencyMap.replace(prev, frequency - 1, frequency);
-      i++;
+    for (int i = 0; i < input.length; i++) {
+      numberFrequencyMap.merge(input[i], 1, Integer::sum);
     }
 
-    int[] kMostFrequent =
-        numberFrequencyMap.entrySet().stream()
+    return numberFrequencyMap.entrySet().stream()
             .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
             .limit(k)
             .mapToInt(Map.Entry::getKey)
             .toArray();
-
-    return kMostFrequent;
   }
 }
